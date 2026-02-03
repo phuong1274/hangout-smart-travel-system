3c01b0986f97de5c292c247e3ec016aa24907cba
LamNV <nguyenvietlam290304@gmail.com>
2026-02-03 14:35:18 +0700

    Add common package

---
+++ HSTS.BE/HSTS.Application/Common/LoggingInterfaces/ILoggingService.cs
@@ -0,0 +1,18 @@
+﻿namespace SEP490.Application.Common.LoggingInterfaces
+{
+    public interface ILoggingService
+    {
+        Task LogActivityAsync(string logContent);
+        Task LogActivityAsync(string logContent, Guid? objectGuid, Guid? userId);
+
+        Task LogErrorAsync(string exception);
+        Task LogErrorAsync(string exception, string positionError);
+        Task LogErrorAsync(string exception, string positionError, Guid? objectGuid, Guid? userId);
+
+        Task LogHistoryAsync(string action);
+        Task LogHistoryAsync(string action, Guid? objectGuid, Guid? userId);
+
+        Task LogLogin(Guid userId);
+        Task LogLogout(Guid userId);
+    }
+}
