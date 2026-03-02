d85768dc806ddc7d8cbe5b280cae269394e00f4e
KenMagic <hoanganhbvb@gmail.com>
2026-03-02 14:37:03 +0700

    [fix] Delete location and its medias

--- HSTS.BE/HSTS.Application/Locations/Commands/DeleteLocationCommand.cs
+++ HSTS.BE/HSTS.Application/Locations/Commands/DeleteLocationCommand.cs
@@ -2,6 +2,7 @@ using ErrorOr;
 using MediatR;
 using HSTS.Application.Interfaces;
 using HSTS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using static HSTS.Application.Interfaces.IRepository;
 
 namespace HSTS.Application.Locations.Commands
@@ -10,25 +11,40 @@ namespace HSTS.Application.Locations.Commands
 
     public class DeleteLocationCommandHandler : IRequestHandler<DeleteLocationCommand, ErrorOr<Deleted>>
     {
-        private readonly IRepository<Location> _repository;
+        private readonly IRepository<Location> _locationRepository;
+        private readonly IRepository<LocationMedia> _mediaRepository;
 
-        public DeleteLocationCommandHandler(IRepository<Location> repository)
+        public DeleteLocationCommandHandler(
+            IRepository<Location> locationRepository,
+            IRepository<LocationMedia> mediaRepository)
         {
-            _repository = repository;
+            _locationRepository = locationRepository;
+            _mediaRepository = mediaRepository;
         }
 
         public async Task<ErrorOr<Deleted>> Handle(DeleteLocationCommand request, CancellationToken cancellationToken)
         {
-            var location = await _repository.GetAsync(request.Id, cancellationToken);
+            var location = await _locationRepository.GetAsync(request.Id, cancellationToken);
 
             if (location == null || location.IsDeleted)
             {
                 return Error.NotFound("Location.NotFound", $"Location with ID {request.Id} not found.");
             }
 
+            // Soft-delete the location
             location.IsDeleted = true;
+            await _locationRepository.UpdateAsync(location, cancellationToken);
 
-            await _repository.UpdateAsync(location, cancellationToken);
+            // Soft-delete all associated media
+            var medias = await _mediaRepository.Query()
+                .Where(m => m.LocationId == request.Id && !m.IsDeleted)
+                .ToListAsync(cancellationToken);
+
+            foreach (var media in medias)
+            {
+                media.IsDeleted = true;
+                await _mediaRepository.UpdateAsync(media, cancellationToken);
+            }
 
             return Result.Deleted;
         }
